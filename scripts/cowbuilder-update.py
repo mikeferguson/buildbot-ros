d319f61a37eb6dbc56b4ec20861a20f8a37b7a18
Michael Ferguson <fergs@unboundedrobotics.com>
2014-01-30 12:12:27 -0800

    fixes cowbuilder part of parallel build problems

--- scripts/cowbuilder-update.py
+++ scripts/cowbuilder-update.py
@@ -6,10 +6,54 @@ from __future__ import print_function
 import sys
 import os
 import subprocess
+import time
 
 # A bit hacky, but do this rather than redefine the function.
 # Has to be in testbuild, as we only copy testbuild to pbuilder.
 from testbuild import call
+
+import random
+file_num = random.randrange(100000)
+
+## @brief Returns whether we could lock the file
+def get_lock(distro, arch):
+    global file_num
+    if os.path.isfile('/tmp/buildbot_'+distro+'_'+arch+'_lock'):
+        # check if our file_num is in the file
+        try:
+            with open('/tmp/buildbot_'+distro+'_'+arch+'_lock') as f:
+                value = int(f.read().rstrip())
+                if value == file_num:
+                    return True
+                else:
+                    # somebody already has a lock
+                    return False
+        except:
+            return False
+    else:
+        # try to put our file_num in the lock file:
+        with open('/tmp/buildbot_'+distro+'_'+arch+'_lock', 'w') as f:
+            f.write(str(file_num))
+        # wait a moment
+        time.sleep(0.05)
+        # check that we got the lock
+        return get_lock(distro, arch)
+
+def release_lock(distro, arch):
+    if os.path.isfile('/tmp/buildbot_'+distro+'_'+arch+'_lock'):
+        # check if our file_num is in the file
+        try:
+            with open('/tmp/buildbot_'+distro+'_'+arch+'_lock') as f:
+                value = int(f.read().rstrip())
+                if value == file_num:
+                    os.remove('/tmp/buildbot_'+distro+'_'+arch+'_lock')
+                    return True
+                else:
+                    # somebody already has a lock
+                    return False
+        except:
+            return False
+    return False
 
 ## @brief Returns the basepath of the cowbuilder
 ## @param distro The UBUNTU distribution (for instance, 'precise')
@@ -31,6 +75,10 @@ def getKeyCommands(keys):
 ## @param arch The architecture (for instance, 'amd64')
 ## @param keys List of keys to get
 def make_cowbuilder(distro, arch, keys):
+    print('(' + str(time.time()) +')Getting lock on cowbuilder')
+    while not get_lock(distro, arch):
+        time.sleep(1.0)
+    print('(' + str(time.time()) +')Got lock!')
     if not os.path.exists(basepath(distro, arch)):
         # create the cowbuilder
         call(['sudo', 'cowbuilder', '--create',
@@ -76,7 +124,10 @@ if __name__=="__main__":
         exit(-1)
     distro = sys.argv[1]
     arch = sys.argv[2]
-    if len(sys.argv) > 3:
-        make_cowbuilder(distro, arch, sys.argv[3:])
-    else:
-        make_cowbuilder(distro, arch, [])
+    try:
+        if len(sys.argv) > 3:
+            make_cowbuilder(distro, arch, sys.argv[3:])
+        else:
+            make_cowbuilder(distro, arch, [])
+    finally:
+        release_lock(distro, arch)
